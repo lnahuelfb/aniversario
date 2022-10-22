5a46171b8207f5d8992598d4d773d4aab73a576d
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-10-22 17:16:02 -0300

    Terminada la pagina base

--- src/data.ts
+++ src/data.ts
@@ -2,46 +2,75 @@ import { messages } from './intefaces'
 import a from './assets/WhatsApp Image 2021-12-25 at 3.25.52 AM.jpeg'
 import s from './assets/WhatsApp Image 2022-02-12 at 10.04.18.jpeg'
 import d from './assets/WhatsApp Image 2022-02-22 at 11.00.35 PM.jpeg'
+import f from './assets/WhatsApp Image 2022-02-28 at 1.25.32 AM.jpeg'
+import g from './assets/WhatsApp Image 2022-02-28 at 1.26.12 AM.jpeg'
+import h from './assets/WhatsApp Image 2022-05-22 at 10.38.45 PM.jpeg'
+import j from './assets/WhatsApp Image 2022-05-22 at 10.39.02 PM.jpeg'
+import k from './assets/WhatsApp Image 2022-05-22 at 10.39.13 PM.jpeg'
+import l from './assets/WhatsApp Image 2022-05-22 at 5.14.39 PM.jpeg'
+import z from './assets/WhatsApp Image 2022-08-16 at 1.34.04 PM.jpeg'
+import HyN from './assets/tumblr_p8as5vXzSV1wegj6so1_1280-1024x576.jpg'
+import huTao from '/Hu Tao.png'
 
 export const data: messages[] = [
   {
-    title: "Feliz primer año!",
+    title: "¡Feliz primer año!",
     message: "¡Hoy cumplimos un año de que comenzó nuestra hermosa relación!",
     image: a
   },
   {
     title: "Gracias por todo",
-    message: "Gracias por todo, te amo, mucho!",
+    message: "Quería comenzar agradeciendote todo lo que hiciste por mí en este tiempo.",
     image: s
   },
   {
-    title: "Lo unico que puede hacer mas grande a Bokita",
+    title: "Siempre estuviste",
+    message: "Cuando te necesité siempre estuviste para alegrarme los días.",
+    image: f
+  },
+  {
+    title: "Por todo el amor que me das",
+    message: "Desde que comenzamos a salir, siempre me sentí muy amado por vos",
+    image: h,
+  },
+  {
+    title: "Por todos esos momentos que vivimos acompañados",
+    message: "Aunque no tenga que ver con nuestra relacion, siempre hiciste especial estas juntadas con los carpinchos",
+    image: g,
+  },
+  {
+    title: "Por los eventos que fuimos, y que iremos juntos",
+    message: "Amé tu cosplay jajaja 💕",
+    image: j,
+  },
+  {
+    title: "Por creer y confiar en mí",
+    message: "Se que tengo muchas cosas que mejorar, y confías en que lo haré. Me voy a esforzar en no decepcionarte",
+    image: k,
+  },
+  {
+    title: "Por hacer que todo sea especial",
+    message: "Todo con vos tiene un gustito diferente, amo compartir hobbie con vos, amo como se siente ver JoJo's a tu lado.",
+    image: l,
+  },
+  {
+    title: "Por todo lo que se viene 💕",
+    message: "Este es solo el primer año juntos. Estoy seguro que se vendrán muchos aniversarios mas, y mucho mejores.",
+    image: z,
+  },
+  {
+    title: "Te amo ❤",
+    message: "Gracias por todo lo que me haces sentir, amarte es lo mas hermoso que me pasó",
+    image: huTao,
+  },
+  {
+    title: "Espero que hayas disfrutado de este regalito ❤",
+    message: "",
+    image: HyN,
+  },
+  {
+    title: "PD: Lo unico que puede hacer mas grande a Bokita",
     message: "Sos vos con el buzo de Bokita",
-    image: d
-  },
-  {
-    title: "a",
-    message: "a",
-    image: d
-  },
-  {
-    title: "a",
-    message: "a",
-    image: d
-  },
-  {
-    title: "a",
-    message: "a",
-    image: d
-  },
-  {
-    title: "a",
-    message: "a",
-    image: d
-  },
-  {
-    title: "a",
-    message: "a",
     image: d
   }
 ]
